8f708e42c70aeb44886f98949f42b2ac1107426f
Nuno Cunha <openaizinho@gmail.com>
2026-03-16 18:01:57 +0000

    Update lib.rs

--- blockchain-core/pallets/reputation/src/lib.rs
+++ blockchain-core/pallets/reputation/src/lib.rs
@@ -1,111 +1,22 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-
 pub use pallet::*;
-
-#[polkadot_sdk::frame_support::pallet]
+#[frame_support::pallet]
 pub mod pallet {
-    use polkadot_sdk::frame_support::pallet_prelude::*;
-    use polkadot_sdk::frame_system::pallet_prelude::*;
-    use polkadot_sdk::sp_std::vec::Vec;
-
-    const MAX_REPUTATION: u32 = 1_000_000;
-
-    #[derive(Encode, Decode, Clone, Copy, PartialEq, Eq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
-    pub enum ContributionType {
-        Construction,
-        Agriculture,
-        Energy,
-        Governance,
-        Health,
-        Logistics,
-    }
-
-    #[derive(Encode, Decode, Clone, Copy, PartialEq, Eq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
-    pub enum SubContributionType {
-        Painting, Carpentry, Masonry, Electrical, Plumbing,
-        CropFarming, Livestock, Horticulture,
-        Solar, Wind, Hydro,
-        Medical, Nursing, FirstAid,
-        Transport, Storage, Distribution,
-        Coordination, Mediation, Planning,
-    }
-
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
     #[pallet::config]
-    pub trait Config: polkadot_sdk::frame_system::Config {
-        type RuntimeEvent: From<Event<Self>> + IsType<<Self as polkadot_sdk::frame_system::Config>::RuntimeEvent>;
+    pub trait Config: frame_system::Config {
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
     }
-
     #[pallet::pallet]
     pub struct Pallet<T>(_);
-
-    #[pallet::storage]
-    pub type Admin<T: Config> = StorageValue<_, T::AccountId>;
-
-    #[pallet::storage]
-    pub type Reputation<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, u32, ValueQuery>;
-
-    #[pallet::storage]
-    pub type ReputationByType<T: Config> = StorageDoubleMap<
-        _,
-        Blake2_128Concat, T::AccountId,
-        Blake2_128Concat, ContributionType,
-        u32,
-        ValueQuery
-    >;
-
-    #[pallet::storage]
-    pub type BuildcoinBalance<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, u128, ValueQuery>;
-
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
-    // CORREÇÃO: Removido o derive manual. O SDK 2026 trata disto automaticamente.
     pub enum Event<T: Config> {
-        ContributionRecorded(T::AccountId, ContributionType, u32),
-        ReputationReduced(T::AccountId, ContributionType, u32),
-        ReputationReset(T::AccountId),
-        AdminSet(T::AccountId),
-        BuildcoinMinted(T::AccountId, u128),
+        ReputationUpdated(T::AccountId, u32),
     }
-
-    #[pallet::error]
-    pub enum Error<T> {
-        Overflow,
-        Underflow,
-        MaxReputationReached,
-        NotAdmin,
-    }
-
+    #[pallet::hooks]
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
     #[pallet::call]
-    impl<T: Config> Pallet<T> {
-        #[pallet::call_index(0)]
-        #[pallet::weight(Weight::default())]
-        pub fn set_admin(origin: OriginFor<T>, new_admin: T::AccountId) -> DispatchResult {
-            ensure_root(origin)?; 
-            Admin::<T>::put(&new_admin);
-            Self::deposit_event(Event::AdminSet(new_admin));
-            Ok(())
-        }
-
-        #[pallet::call_index(1)]
-        #[pallet::weight(Weight::default())]
-        pub fn record_contribution(
-            origin: OriginFor<T>,
-            contribution: ContributionType,
-            amount: u32,
-        ) -> DispatchResult {
-            let who = ensure_signed(origin)?;
-            ReputationByType::<T>::try_mutate(&who, contribution, |rep| -> Result<(), Error<T>> {
-                *rep = rep.checked_add(amount).ok_or(Error::<T>::Overflow)?;
-                Ok(())
-            })?;
-            Self::deposit_event(Event::ContributionRecorded(who, contribution, amount));
-            Ok(())
-        }
-    }
-
-    impl<T: Config> Pallet<T> {
-        pub fn select_top_experts(contribution_type: Vec<u8>) -> Vec<(T::AccountId, u32)> {
-            Vec::new() // Placeholder simplificado para compilação
-        }
-    }
+    impl<T: Config> Pallet<T> {}
 }
