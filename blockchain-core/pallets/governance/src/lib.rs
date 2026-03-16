905aabe7a0225bfe5b0032ef8ce1b83c8adbfe20
Nuno Cunha <openaizinho@gmail.com>
2026-03-16 10:30:15 +0000

    Update lib.rs

--- blockchain-core/pallets/governance/src/lib.rs
+++ blockchain-core/pallets/governance/src/lib.rs
@@ -1,22 +1,40 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+
 pub use pallet::*;
+
 #[polkadot_sdk::frame_support::pallet]
 pub mod pallet {
+    // Usamos os caminhos absolutos para garantir que o compilador não se perde
     use polkadot_sdk::frame_support::pallet_prelude::*;
     use polkadot_sdk::frame_system::pallet_prelude::*;
+
     #[pallet::config]
     pub trait Config: polkadot_sdk::frame_system::Config {
         type RuntimeEvent: From<Event<Self>> + IsType<<Self as polkadot_sdk::frame_system::Config>::RuntimeEvent>;
     }
+
     #[pallet::pallet]
     pub struct Pallet<T>(_);
+
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    // Adicionamos as derivações necessárias para o Runtime
+    #[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, TypeInfo)]
     pub enum Event<T: Config> {
         ProposalCreated(u32),
     }
+
     #[pallet::hooks]
     impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+
     #[pallet::call]
-    impl<T: Config> Pallet<T> {}
+    impl<T: Config> Pallet<T> {
+        #[pallet::call_index(0)]
+        #[pallet::weight(Weight::default())]
+        pub fn create_proposal(origin: OriginFor<T>, id: u32) -> DispatchResult {
+            ensure_signed(origin)?;
+            Self::deposit_event(Event::ProposalCreated(id));
+            Ok(())
+        }
+    }
 }
