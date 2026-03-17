392a9d4345c0dde17bd199c4448efcffc31d017c
Nuno Cunha <openaizinho@gmail.com>
2026-03-17 20:19:19 +0000

    Update lib.rs

--- blockchain-core/pallets/projects/src/lib.rs
+++ blockchain-core/pallets/projects/src/lib.rs
@@ -1,22 +1,43 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+
 pub use pallet::*;
+
 #[frame_support::pallet]
 pub mod pallet {
     use frame_support::pallet_prelude::*;
     use frame_system::pallet_prelude::*;
+
     #[pallet::config]
     pub trait Config: frame_system::Config {
+        /// O tipo de evento que a blockchain vai emitir.
         type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
     }
+
     #[pallet::pallet]
     pub struct Pallet<T>(_);
+
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
-        ProjectCreated(T::AccountId, u32),
+        /// Evento disparado quando um novo projeto é criado.
+        /// [id_do_criador, id_do_projeto]
+        ProjectCreated { creator: T::AccountId, project_id: u32 },
     }
+
+    // Estrutura base de erros para a palete de projetos
+    #[pallet::error]
+    pub enum Error<T> {
+        /// Exemplo: O projeto já existe
+        ProjectAlreadyExists,
+        /// Exemplo: Limite de projetos atingido
+        MaxProjectsReached,
+    }
+
     #[pallet::hooks]
     impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+
     #[pallet::call]
-    impl<T: Config> Pallet<T> {}
+    impl<T: Config> Pallet<T> {
+        // As tuas funções (extrinsics) para criar e financiar projetos entram aqui.
+    }
 }
